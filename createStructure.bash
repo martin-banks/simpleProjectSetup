723cbfee5f615b82f9461b9e399ab11c553dbf95
Banks, Martin <anotherbanksy@gmail.com>
2016-11-25 12:25:36 +1100

    additional files into createstructure

--- createStructure.bash
+++ createStructure.bash
@@ -7,27 +7,32 @@ mkdir -p images
 
 mkdir -p src/${styles%%}
 mkdir -p src/${scripts%%}
+#mkdir -p src/${scripts%%}/content
 
 mkdir -p dist/${styles%%}
 mkdir -p dist/${scripts%%}
 
 touch src/${scripts%%}/app.js
 touch src/${scripts%%}/state.js
+touch src/${scripts%%}/appDesktop.js
+touch src/${scripts%%}/appMobile.js
+touch src/${scripts%%}/templates.js
+touch src/${scripts%%}/content.js
 
-touch src/${styles%%}/global.sass
+touch src/${styles%%}/global.css
 
 touch dist/preview.html
 
-echo -e "module.exports = {\n\n}" >> src/${scripts%%}/state.js
+echo -e "module.exports = {\ncontent: require('./content.js'),\n}" >> src/${scripts%%}/state.js
+echo -e "module.exports = {\n\n}" >> src/${scripts%%}/content.js
 
 
 echo -e "(()=>{" >> src/${scripts%%}/app.js
-echo -e "	const state = require('./state')" >> src/${scripts%%}/app.js
+echo -e "const state = require('./state')" >> src/${scripts%%}/app.js
 echo -e "\n\n})()" >> src/${scripts%%}/app.js
 
 npm init -y
 
-# npm i --save browserify
-# npm i --save watchify
-# npm i --save babelify
-# npm i --save babel-preset-es2015
+#npm i --save-dev browserify watchify babelify babel-preset-es2015
+
+ls -al
